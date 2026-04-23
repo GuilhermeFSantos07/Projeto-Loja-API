75440e20bbff95074cb5461ab79bdf7e2d0d9b4a
Guilherme Farias dos Santos <guilherme.f.santos01702@gmail.com>
2026-04-23 13:01:14 -0400

    atualização das rotas para o CRUD

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
-import { criarProduto } from "../controllers/ProductController.js";
+import { 
+    criarProduto,
+    listarProdutos,
+    atualizarProdutos,
+    deletarProduto
+ } from "../controllers/ProductController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = Router();
+
 router.post('/', authMiddleware, criarProduto);
+router.get('/', authMiddleware, listarProdutos);
+router.put('/:id', authMiddleware, atualizarProdutos);
+router.delete('/:id', authMiddleware, deletarProduto);
 
 export default router;
