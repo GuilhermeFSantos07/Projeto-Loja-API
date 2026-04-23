200f57da45b054d0697320876b56a64d5156b00e
Guilherme Farias dos Santos <guilherme.f.santos01702@gmail.com>
2026-04-23 12:52:23 -0400

    Criação do Read, Update e Delete

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -22,3 +22,40 @@ export const criarProduto = async (req, res) => {
         res.status(500).json({message: "Erro ao criar produto", erro: error.message});
     }
 };
+
+export const listarProdutos = async (req, res) => {
+    try{
+        const produtos = await Product.find().populate('criadoPor', nome);
+        res.status(200).json(produtos);
+    }catch(error){
+        res.status(500).json({message: "Erro ao buscar produtos", erro: error.message});
+    }
+};
+
+export const atualizarProdutos = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const dadosAtualizados = req.body;
+
+        const produto = await Product.findByIdAndUpdate(id, dadosAtualizados, {new: true});
+
+        if(!produto) return res.status(404).json({message: "Produto não encontrado"});
+
+        res.status(200).json({message: "Produto atualizado", produto});
+    }catch(error){
+        res.status(500).json({message: "Erro ao atualizar produto", erro: error.message});
+    }
+};
+
+export const deletarProduto = async (res, req) => {
+    try{
+        const {id} = req.params;
+
+        const produtoDeletado = await Product.findByIdAndDelete(id);
+        if(!produtoDeletado) return res.status(404).json({message: "Produto não encontrado"});
+
+        res.status(200).json({message: "Produto deletado com sucesso"});
+    }catch(error){
+        res.status(500).json({message: "Erro ao deletar produto", erro: error.message});
+    }
+};
