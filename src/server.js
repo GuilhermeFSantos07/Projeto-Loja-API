a0db6c4bdb5933c85eefcd4e595f0e15ab9412c5
Guilherme Farias dos Santos <guilherme.f.santos01702@gmail.com>
2026-04-23 15:04:25 -0400

    configuração para testes

--- src/server.js
+++ src/server.js
@@ -20,7 +20,11 @@ app.get('/', (req, res) => {
     res.send("API está online");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
+if(process.env.NODE_ENV !== 'test') {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando na porta ${PORT}`);
+    });
+}
+
+export default app;
