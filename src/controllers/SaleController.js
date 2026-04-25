3c819b31cfd0c39869d2148a7780c6f6cfffc5e9
Guilherme Farias dos Santos <guilherme.f.santos01702@gmail.com>
2026-04-25 01:09:09 -0400

    criação do controlador de vendas

---
+++ src/controllers/SaleController.js
@@ -0,0 +1,26 @@
+import Sale from "../models/Sale.js";
+import Product from '../models/Product.js';
+
+export const registrarVenda = async (req, res) => {
+    try{
+        const {itens, valorTotal, metodoPagamento, desconto} = req.body;
+
+        const novaVenda = new Sale({
+            valorTotal,
+            metodoPagamento,
+            desconto,
+            vendedor: req.user.id,
+            itens
+        });
+        await novaVenda.save();
+
+        for (let item of itens){
+            await Product.findByIdAndUpdate(item.produto.Id, {
+                $inc : {quantidade: -item.quantidadeVendida}
+            });
+        }
+        res.status(201).json({message: "Venda registrada e estoque atualizado", venda: novaVenda});
+    }catch(error){
+        res.status(500).json({message: "Erro ao registrar venda", erro: error.message});
+    }
+};
