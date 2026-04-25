f3f33514d04ec2da04a10a69071ca398f7adc21a
Guilherme Farias dos Santos <guilherme.f.santos01702@gmail.com>
2026-04-25 15:40:24 -0400

    Adicionando o get das vendas

--- src/routes/saleRoutes.js
+++ src/routes/saleRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { registrarVenda } from "../controllers/SaleController.js";
+import { registrarVenda, listarVendas } from "../controllers/SaleController.js";
 import {authMiddleware} from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
 router.post('/', authMiddleware, registrarVenda);
+router.get('/', authMiddleware, listarVendas);
 
 export default router;
